1d93d5abb4ac945eb7341c61eb92d5910e881435
hadeelhhawajreh <hadeelhhawajreh@gmail.com>
2021-05-01 14:08:33 +0300

    Finish design and stylling

--- app.js
+++ app.js
@@ -1,88 +1,109 @@
 'use strict';
-var allDevices = [];
 var tHeader = [' Decive Name ', ' Quantity', ' unit Price ', ' Category'];
-// - Whenever we add a new device, you should generate the device price as an integer random number between 350 and 750.
-var totalprice=0;
-if(localStorage.getItem('devices')){
-    var data=JSON.parse(localStorage.getItem('devices'));
-    for (var i = 0; i < data.length; i++) {
-        new Device(data[i].name,data[i].category,data[i].random,data[i].quantity);
-    }
-}
+// - Whenever we add a new device, you should generate the device price as
+// an integer random number between 350 and 750.
 
+var totalprice = 0;
 function Device(name, category, quantity) {
     this.name = name;
     this.category = category;
     this.quantity = quantity;
-    this.random = 0;
-    allDevices.push(this);
+    this.rannd = 0;
+    Device.allDevices.push(this);
+    localStorage.setItem('all',JSON.stringify(Device.allDevices));
+
 }
+Device.allDevices = [];
 
 Device.prototype.priceRandom = function () {
-    this.random = Math.floor(Math.random() * (750 - 350 + 1) + 350);
-    return this.random;
-
+    this.rannd = Math.floor(Math.random() * (750 - 350 + 1) + 350);
+    return (this.rannd);
 }
 
-function total(){
-var totalprice=document.getElementById('totalprice');
-for (var i = 0; i < allDevices.length; i++) {
-    totalprice+=this.random;
-    
-}
-totalprice.textContent=totalprice();
 
+Device.prototype.total = function () {
+    console.log('inside function ');
+    console.log(this.quantity);
+    let x = (this.quantity) * (this.priceRandom());
+    return x;
 }
-var parent = document.getElementById('table-id');
-var table = document.createElement('table');
-parent.appendChild(table);
-tHead();
-function tHead() {
-    var tr = document.createElement('tr');
-    table.appendChild(tr);
-    var th;
-    for (var i = 0; i < tHeader.length; i++) {
+
+
+
+let table = document.getElementById('table-id');
+let tr = document.createElement('tr');
+
+let th = document.createElement('th');
+let td = document.createElement('td');
+function thead() {
+    tr = document.createElement('tr');
+    for (let index = 0; index < tHeader.length; index++) {
         th = document.createElement('th');
-        th.textContent = tHeader[i];
+        th.textContent = tHeader[index];
         tr.appendChild(th);
     }
-}
-Device.prototype.renerTable = function () {
-    var tr = document.createElement('tr');
+
     table.appendChild(tr);
-    for (var i = 0; i < allDevices.length; i++) {
-    var td1 = document.createElement('td');
-    td1.textContent = allDevices[i].name;
+
+}
+thead();
+
+Device.prototype.tBody= function () {
+    console.log('inside body');
+
+    tr = document.createElement('tr');
+
+    let td1 = document.createElement('td');
+    td1.textContent = this.name;
     tr.appendChild(td1);
-    var td2 = document.createElement('td');
-    td2.textContent = allDevices[i].category;
+    let td2 = document.createElement('td');
+    td2.textContent = this.quantity;
     tr.appendChild(td2);
-
-    var td3 = document.createElement('td');
-    td3.textContent = allDevices[i].priceRandom();
+    let td3 = document.createElement('td');
+    td3.textContent = this.total();
     tr.appendChild(td3);
-    
-    var td4 = document.createElement('td');
-    td4.textContent = allDevices[i].quantity;
+    let td4 = document.createElement('td');
+    td4.textContent = this.category;
     tr.appendChild(td4);
+    table.appendChild(tr);
+}
 
+console.log('Device.allDevices',Device.allDevices);
+
+
+function renderAll(params) {
+    for (let j = 0; j < Device.allDevices.length; j++) {
+        Device.allDevices[j].priceRandom();
+        Device.allDevices[j].total();
+        Device.allDevices[j].tBody();
+        
     }
-
 }
 
-var form = document.getElementById('form-id');
-form.addEventListener('submit', formSubmit);
-function formSubmit(event) {
-    event.preventDefault();
-    var itemName = event.target.itemName.value;
-    var categ = event.target.categ.value;
-    var quantitiy = event.target.quantitiy.value;
-    var devcie = new Device(itemName, categ, quantitiy);
-    devcie.priceRandom();
-    devcie.renerTable();
-localStorage.setItem('devices',JSON.stringify(allDevices));
+renderAll();
+
+let form=document.getElementById('form-id');
+form.addEventListener('submit',submitHandler);
+
+function submitHandler(e) {
+    e.preventDefault();
+    let item=e.target.item.value;
+    let cat=e.target.cat.value;
+    let qua=e.target.qau.value;
+    console.log('form submit',item,cat,Number(qua));
+    new Device(item,cat,Number(qua)); 
+    table.textContent='';
+    thead();
+    renderAll();
 }
-for (var i = 0; i < allDevices.length; i++) {
-    allDevices[i].priceRandom();
-    allDevices[i].renerTable();
+
+let data=JSON.parse(localStorage.getItem('all'));
+console.log(data);
+function getFromLS() {
+    for (let i = 0; i < data.length; i++) {
+       new Device(data[i].name,data[i].category,data[i].quantity)
+        
+    }
+    renderAll();
 }
+getFromLS();
